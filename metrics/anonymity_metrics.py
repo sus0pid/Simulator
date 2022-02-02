fc7685682df26b839f9a56d778b8aad4a776dbe3
sus0pid <s2136500@ed.ac.uk>
2022-02-02 14:00:42 +0000

    absolute value of epislon

--- metrics/anonymity_metrics.py
+++ metrics/anonymity_metrics.py
@@ -32,14 +32,14 @@ def getUnlinkability(data):
 			if not float(prB) == 0.0:
 				ratio = float(prA) / float(prB)
 				if not ratio == 0.0:
-					epsilon.append(math.log(ratio))
+					epsilon.append(abs(math.log(ratio)))
 			else:
 				dlts += 1
 		elif label == 2:
 			if not float(prA) == 0.0:
 				ratio = float(prB) / float(prA)
 				if not ratio == 0.0:
-					epsilon.append(math.log(ratio))
+					epsilon.append(abs(math.log(ratio)))
 			else:
 				dlts += 1
 		else:
